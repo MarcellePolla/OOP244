b24d438773da55a55ff3cf5f9b6665e39f680ea1
MarcellePolla <cellypolla@gmail.com>
2019-09-21 20:45:46 -0400

    Update seneGraph.cpp

--- workshops/ws1/at_home/seneGraph.cpp
+++ workshops/ws1/at_home/seneGraph.cpp
@@ -1,16 +1,3 @@
-/***********************************************************************
-// OOP244 Workshop 1: Compiling modular source code
-// File	seneGraph.cpp
-// Version 1.0
-// Date	2019/05/11
-// Author	Marcelle Polla
-// Section B
-// Student Number 126654185
-// email mpolla1@myseneca.ca
-// Description
-// This is the main for seneGraph 
-/////////////////////////////////////////////////////////////////
-***********************************************************************/
 #include <iostream>
 #include "graph.h"
 #include "tools.h"
