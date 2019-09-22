ea086346a3b637bf04b096d3b2d9132ba0015a76
MarcellePolla <cellypolla@gmail.com>
2019-09-21 21:33:33 -0400

    Update SuperHero.h

--- workshops/ws7/at_home/SuperHero.h
+++ workshops/ws7/at_home/SuperHero.h
@@ -2,7 +2,6 @@
 // OOP244 Workshop 7: Derived Classes
 // File SuperHero.h
 // Date July 17th
-// Author Marcelle Polla
 // Description
 // This is the header file for the SuperHero module.
 // It contains the prototypes for the SuperHero class.
