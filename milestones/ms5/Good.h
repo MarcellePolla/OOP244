d23c93f54a64f2f31d8f964267a937893b054345
MarcellePolla <cellypolla@gmail.com>
2019-09-21 21:53:32 -0400

    Update Good.h

--- milestones/ms5/Good.h
+++ milestones/ms5/Good.h
@@ -2,7 +2,6 @@
 // OOP244 Aid Management Application : Milestone 5
 // File Good.h
 // Date Aug 3rd
-// Author Marcelle Polla
 // Description
 // This is the header file for the Good module.
 // It contains the prototypes for the Good class.
