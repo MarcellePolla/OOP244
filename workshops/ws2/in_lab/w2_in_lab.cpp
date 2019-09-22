a909789fedc76487fa0917ac11b28de8245a2141
MarcellePolla <cellypolla@gmail.com>
2019-09-21 21:01:03 -0400

    Add files via upload

---
+++ workshops/ws2/in_lab/w2_in_lab.cpp
@@ -0,0 +1,76 @@
+/***********************************************************
+// Workshop 2: Dynamic Memory
+// File w2_in_lab.cpp
+// Version 2.0
+// Date May 23rd
+// Author Marcelle Polla
+// Description
+// This program stores and displays the names of Kingdoms 
+// and total population.
+//
+// Revision History
+///////////////////////////////////////////////////////////
+// Name     Date    Reason
+// 
+///////////////////////////////////////////////////////////
+***********************************************************/
+#include <iostream>
+#include "Kingdom.h"
+#include "Kingdom.h" // intentional
+
+using namespace std;
+using namespace sict;
+
+void read(sict::Kingdom&);
+
+int main() {
+	int count = 0; // the number of kingdoms in the array
+
+	//Pointer declaration and initialization
+	Kingdom* pKingdom = nullptr;
+
+
+	cout << "==========" << endl
+		 << "Input data" << endl
+		 << "==========" << endl
+		 << "Enter the number of Kingdoms: ";
+	cin >> count;
+	cin.ignore();
+
+	if (count < 1) return 1;
+
+	// Dynamic memory allocation
+	pKingdom = new Kingdom[count];
+
+	for (int i = 0; i < count; ++i) {
+		cout << "Kingdom #" << i + 1 << ": " << endl;
+		//Accepts user input for Kingdom information
+		read(pKingdom[i]);
+	}
+	cout << "==========" << endl << endl;
+
+	// testing that "display(...)" works
+	cout << "------------------------------" << endl
+		 << "The 2nd kingdom entered is" << endl
+		 << "------------------------------" << endl;
+	sict::display(pKingdom[1]);
+	cout << "------------------------------" << endl << endl;
+
+	// Dynamic memory deallocation
+	delete [] pKingdom;
+	pKingdom = nullptr;
+
+	return 0;
+}
+
+// read accepts data for a Kingdom from standard input
+//
+void read(sict::Kingdom& kingdom) {
+
+	cout << "Enter the name of the Kingdom: ";
+	cin.get(kingdom.m_name, 32, '\n');
+	cin.ignore(2000, '\n');
+	cout << "Enter the number of people living in " << kingdom.m_name << ": ";
+	cin >> kingdom.m_population;
+	cin.ignore(2000, '\n');
+}
