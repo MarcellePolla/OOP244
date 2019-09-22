505ae0b3beca218901f9a1a6b121803d46473876
MarcellePolla <cellypolla@gmail.com>
2019-09-21 21:44:58 -0400

    Update Error.h

--- milestones/ms2/Error.h
+++ milestones/ms2/Error.h
@@ -2,7 +2,6 @@
 // OOP244 Aid Management Application : Milestone 2
 // File Error.h
 // Date July 15th
-// Author Marcelle Polla
 // Description
 // This is the header file for the Error module.
 // It contains the prototypes for the Error class.
