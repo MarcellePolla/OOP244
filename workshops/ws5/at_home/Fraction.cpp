9403e92163899ad7073355ba1b5819d11170af86
MarcellePolla <cellypolla@gmail.com>
2019-09-21 21:23:51 -0400

    Update Fraction.cpp

--- workshops/ws5/at_home/Fraction.cpp
+++ workshops/ws5/at_home/Fraction.cpp
@@ -3,7 +3,6 @@
 // File Fraction.cpp
 // Version 1.0
 // Date June 15th
-// Author Marcelle Polla
 // Description
 // This is the implementation file for the Fraction module.
 // It contains the function definitions for the Fraction class.
